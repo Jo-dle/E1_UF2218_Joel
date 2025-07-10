80033ccdc89eaa5c1d6a617280f0861721e8f6f6
Jo-dle <jramoszurita@gmail.com>
2025-07-10 09:40:28 +0100

    boceto de index2

---
+++ vistas/index2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang=es>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Instancia</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <body>
+        <h2>Instancia</h2>
+                                                                                                fecha:dd/mm/aaaa
+        [nombre][apellido] con DNI [dni] y correo electrónico [email]
+        Expone: [campo para insertar texto grande]
+        Solicita:  [campo para insertar texto grande]
+    </body>
